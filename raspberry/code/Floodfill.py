a654ec66ea92236f9e105b94a1e285e8ad497d97
Jonas Wühr <jonas.wuehr@t-online.de>
2020-09-03 13:26:42 +0100

    buggy pathplanner

--- raspberry/code/Floodfill.py
+++ raspberry/code/Floodfill.py
@@ -3,7 +3,7 @@ import cv2
 
 class Floodfill:
 
-    def __init__(self, fieldNr = 21, driveableThreshold = 1):
+    def __init__(self, fieldNr = 21, driveableThreshold = 0.5):
 
         self.fieldNr = fieldNr
         self.threshold = driveableThreshold
@@ -46,11 +46,11 @@ class Floodfill:
         pixel[2] = self.waypointColor[2]
         self.fields[startPos[1], startPos[0]]= pixel
 
-        pixel = self.fields[5, 15]
+        pixel = self.fields[15, 5]
         pixel[0] = self.waypointColor[0]
         pixel[1] = self.waypointColor[1]
         pixel[2] = self.waypointColor[2]
-        self.fields[5, 15] = pixel
+        self.fields[15, 5] = pixel
 
         #for x in range(0, self.fieldNr):
         #    for y in range(0, self.fieldNr):
@@ -60,7 +60,7 @@ class Floodfill:
 
         stack.append(startPos)
 
-
+        destPos = self.startPos
 
         while len(stack) > 0:
             #print(len(stack))
@@ -68,7 +68,7 @@ class Floodfill:
             position = stack.pop()
             x = position[0]
             y = position[1]
-            if x > 0 and x < self.fieldNr and y > 0 and y < self.fieldNr:
+            if x >= 0 and x < self.fieldNr and y >= 0 and y < self.fieldNr:
                 pixel = self.fields[y, x]
                 #print(pixel)
                 driveable = True
@@ -77,32 +77,76 @@ class Floodfill:
                     #print(pixel)
                 elif pixel[1] == self.waypointColor[1] and pixel[2] == self.waypointColor[2] and x != self.startPos[0] and y != self.startPos[1]:
                     driveable = False
-                    print(pixel)
+                    #print(pixel)
                     destinationFound = True
-                    print(destinationFound)
+                    destPos = (x, y)
+                    #print(destinationFound)
 
                 if driveable and not destinationFound:
                     value = values[x, y]
                     newValue = lastValue + 1
                     #print(lastValue)
-                    if newValue < value:#git pull
-                        # and value == self.fieldNr * self.fieldNr:
+                    if newValue <= value:
                         lastValue = newValue
-                        pixel[0] = (int)(newValue * 127 / (self.fieldNr * self.fieldNr)) + 127
-                        self.fields[y, x] = pixel
+                        #pixel[0] = 255#(int)(newValue * 127 / (self.fieldNr * self.fieldNr)) + 127
+                        #self.fields[y, x] = pixel
                         values[x, y] = newValue
                         #print(self.fields[y, x])
+                        stack.append((x - 1, y - 1))
+                        stack.append((x - 1, y + 1))
+                        stack.append((x - 1, y))
+                        stack.append((x + 1, y - 1))
+                        stack.append((x + 1, y + 1))
+                        stack.append((x + 1, y))
                         stack.append((x, y + 1))
                         stack.append((x, y - 1))
-                        stack.append((x + 1, y))
-                        stack.append((x - 1, y))
-                        stack.append((x - 1, y - 1))
-                        stack.append((x + 1, y - 1))
-                        stack.append((x - 1, y + 1))
-                        stack.append((x + 1, y + 1))
+
                 #else:
                 #    print(driveable, destinationFound, lastValue)
+        path = []
 
+        x = destPos[0]
+        y = destPos[1]
+        #print(values)
+        while( x != self.startPos[0] and y != self.startPos[1]):
+            valuesAround = []
+            valuesAround.append(values[x, y + 1])
+            valuesAround.append(values[x, y - 1])
+            valuesAround.append(values[x + 1, y])
+            valuesAround.append(values[x - 1, y])
+            valuesAround.append(values[x - 1, y - 1])
+            valuesAround.append(values[x + 1, y - 1])
+            valuesAround.append(values[x - 1, y + 1])
+            valuesAround.append(values[x + 1, y + 1])
+
+            next = valuesAround.index(min(valuesAround))
+
+            if next == 0:
+                y += 1
+            elif next == 1:
+                y -= 1
+            elif next == 2:
+                x += 1
+            elif next == 3:
+                x -= 1
+            elif next == 4:
+                x -= 1
+                y -= 1
+            elif next == 5:
+                x += 1
+                y -= 1
+            elif next == 6:
+                x -= 1
+                y += 1
+            elif next == 7:
+                x += 1
+                y += 1
+
+            pixel = self.fields[y, x]
+            pixel[0] = 255
+            self.fields[y, x] = pixel
+            path.append((x, y))
+        #print(path)
 
         #for x in range(0, self.fieldNr):
         #    for y in range(0, self.fieldNr):
